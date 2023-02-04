49e6072e679a79432f40281cb7dc3d988030fcb5
Alek Novkovski <alek@greatcode2.com>
2023-02-04 20:47:42 +0100

    Build proxy functionality

--- working/server/server.js
+++ working/server/server.js
@@ -4,15 +4,33 @@ const cors = require('cors')
 
 const app = express()
 
+async function proxyMiddleware(req, res, next) {
+	const url = 'https://jsonplaceholder.typicode.com' + req.url
+	try {
+		const request = await fetch(url)
+		req.response = await request.json()
+	}
+	catch (e) {
+		console.log(e)
+	}
+	next()
+}
+
 app.use(cors())
+
+// Proxy endpoints
+app.use('/', proxyMiddleware);
 
 app.get('/', (req, res) => {
 	res.json({
-		helloWorld: true,
+		helloWorld: true
 	})
 })
 
 app.get('*', (req, res) => {
+	if(req.response) {
+		return res.json(req.response)
+	}
 	res.json({
 		404: true,
 	})
