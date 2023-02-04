3f65ce8f5c8948f334a24152ffb5898af392c2e4
Alek Novkovski <alek@greatcode2.com>
2023-02-04 21:32:08 +0100

    Show body and comments on open

--- working/client/src/App.jsx
+++ working/client/src/App.jsx
@@ -17,16 +17,37 @@ function App() {
 	function Post (props) {
 
 		const [isOpen, setIsOpen] = useState(false)
-		useEffect(()=>{
+		const [comments, setComments] = useState([])
+
+		useEffect(()=> {
 			if(isOpen) {
-				console.log('the post is open now')
+				const API_URL = `http://${window.location.hostname}:1338/posts/${props.id}/comments/`
+				console.log(API_URL)
+				fetch(API_URL)
+					.then((t) => t.json())
+					.then((json) => setComments(json))
 			}
 		}, [isOpen])
+
+		const commentsMarkup = comments.map((comment) => {
+			return (<div key={comment.id}>
+				<b>{`${comment.name} | (${comment.email})`}</b>
+				<p>{comment.body}</p>
+			</div>)
+		})
+
+		const openedMarkup = (
+			<article>
+				<p>{props.body}</p>
+				<h3>Comments</h3>
+				{commentsMarkup}
+			</article>
+		)
 
 		return <div key={props.id} data-id={props.id}>
 			<h2 onClick={()=> setIsOpen(true)}>{props.title}</h2>
 			{
-				isOpen ? "open"
+				isOpen ? openedMarkup
 				: "closed"
 			}
 		</div>
@@ -42,7 +63,7 @@ function App() {
 				onChange={(e) => setValue(e.target.value)}
 			/>
 			<div>{data.map((item) => {
-				return <Post key={item.id} id={item.id} title={item.title}/>
+				return <Post key={item.id} id={item.id} title={item.title} body={item.body}/>
 			})}</div>
 		</div>
 	)
